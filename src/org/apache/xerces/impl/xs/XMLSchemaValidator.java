78eb59ae371d4f3a3f711b4e2832fbd5715b2c2e
mukulg <mukulg@13f79535-47bb-0310-9956-ffa450edef68>
2021-10-22 10:17:43 +0000

    committing fix for jira issue XERCESJ-1731
    git-svn-id: https://svn.apache.org/repos/asf/xerces/java/branches/xml-schema-1.1-dev@1894472 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xerces/impl/xs/XMLSchemaValidator.java
+++ src/org/apache/xerces/impl/xs/XMLSchemaValidator.java
@@ -2793,10 +2793,18 @@ public class XMLSchemaValidator extends XMLSchemaValidatorBase implements XMLCom
             // {required} is true matches one of the attribute information items in the element
             // information item's [attributes] as per clause 3.1 above.
             if (currUse.fUse == SchemaSymbols.USE_REQUIRED) {
-                if (!isSpecified)                    
-                    reportSchemaError(
-                        "cvc-complex-type.4",
-                        new Object[] { element.rawname, currDecl.fName });
+                if (!isSpecified) {                    
+                    if (currDecl.fTargetNamespace != null) {
+                       reportSchemaError(
+                                "cvc-complex-type.4_ns",
+                                new Object[] { element.rawname, currDecl.fName, currDecl.fTargetNamespace });      
+                    }
+                    else {
+                       reportSchemaError(
+                                "cvc-complex-type.4",
+                                new Object[] { element.rawname, currDecl.fName });
+                    }                    
+                }
             }
             // if the attribute is not specified, then apply the value constraint
             if (!isSpecified && constType != XSConstants.VC_NONE) {

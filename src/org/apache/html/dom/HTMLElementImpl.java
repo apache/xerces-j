09181acfad03eedb6f43c0cc7c329e586c07d573
Elliotte Rusty Harold <elharo@ibiblio.org>
2025-07-07 07:21:16 -0400

    [XERCESJ-731] correct Javadoc about boolean attributes

--- src/org/apache/html/dom/HTMLElementImpl.java
+++ src/org/apache/html/dom/HTMLElementImpl.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,9 +33,9 @@ import org.w3c.dom.html.HTMLFormElement;
  * the <code>id</code>, <code>title</code>, <code>lang</code>,
  * <code>dir</code> and <code>class</code> attributes. Other elements
  * add their own specific attributes.
- *
+ * 
  * @xerces.internal
- *
+ * 
  * @version $Revision$ $Date$
  * @author <a href="mailto:arkin@exoffice.com">Assaf Arkin</a>
  * @see org.w3c.dom.html.HTMLElement
@@ -51,59 +51,59 @@ public class HTMLElementImpl
      * Constructor required owner document and element tag name. Will be called
      * by the constructor of specific element types but with a known tag name.
      * Assures that the owner document is an HTML element.
-     *
+     * 
      * @param owner The owner HTML document
      * @param tagName The element's tag name
      */
     public HTMLElementImpl( HTMLDocumentImpl owner, String tagName ) {
         super( owner, tagName.toUpperCase(Locale.ENGLISH) );
     }
-
+    
     public String getId() {
         return getAttribute( "id" );
     }
-
+    
     public void setId( String id ) {
         setAttribute( "id", id );
-    }
-
+    }   
+    
     public String getTitle() {
         return getAttribute( "title" );
     }
-
+    
     public void setTitle( String title ) {
         setAttribute( "title", title );
     }
-
+    
     public String getLang() {
         return getAttribute( "lang" );
-    }
-
+    }  
+    
     public void setLang( String lang ) {
         setAttribute( "lang", lang );
     }
-
+    
     public String getDir() {
         return getAttribute( "dir" );
     }
-
+    
     public void setDir( String dir ) {
         setAttribute( "dir", dir );
     }
-
+ 
     public String getClassName() {
         return getAttribute( "class" );
     }
-
+    
     public void setClassName( String className ) {
         setAttribute( "class", className );
     }
-
+    
     /**
      * Convenience method used to translate an attribute value into an integer
      * value. Returns the integer value or zero if the attribute is not a
      * valid numeric string.
-     *
+     * 
      * @param value The value of the attribute
      * @return The integer value, or zero if not a valid numeric string
      */
@@ -115,26 +115,26 @@ public class HTMLElementImpl
             return 0;
         }
     }
-
+    
     /**
      * Convenience method used to translate an attribute value into a boolean
      * value. If the attribute has an associated value (even an empty string),
-     * it is set and true is returned. If the attribute does not exist, false
-     * is returend.
-     *
-     * @param value The value of the attribute
-     * @return True or false depending on whether the attribute has been set
+     * true is returned. If the element does not have this attribute, false
+     * is returned.
+     * 
+     * @param name the name of the attribute
+     * @return true or false depending on whether the attribute has been set
      */
     boolean getBinary( String name ) {
-        return ( getAttributeNode( name ) != null );
+        return getAttributeNode( name ) != null;
     }
-
+    
     /**
      * Convenience method used to set a boolean attribute. If the value is true,
-     * the attribute value is set to the attribute name. For example,
+     * the attribute value is set to the attribute's name. For example,
      * {@code checked="checked"} or {disabled="disabled"}.
      * If the value is false, the attribute is removed. As long as the
-     * attibute is present, regardless of value, HTML considers a boolean attribute to be true.
+     * attribute is present, regardless of value, HTML considers a boolean attribute to be true.
      *
      * @param name the name of the attribute
      * @param value the truth value of the attribute
@@ -159,7 +159,7 @@ public class HTMLElementImpl
         }
         return super.getAttributeNode( localName.toLowerCase(Locale.ENGLISH) );
     }
-
+    
     public String getAttribute( String attrName ) {
         return super.getAttribute( attrName.toLowerCase(Locale.ENGLISH) );
     }
@@ -188,15 +188,15 @@ public class HTMLElementImpl
      * Convenience method used to capitalize a one-off attribute value before it
      * is returned. For example, the align values "LEFT" and "left" will both
      * return as "Left".
-     *
+     * 
      * @param value The value of the attribute
      * @return The capitalized value
      */
     String capitalize( String value ) {
-
+        
         char[]    chars;
         int        i;
-
+        
         // Convert string to charactares. Convert the first one to upper case,
         // the other characters to lower case, and return the converted string.
         chars = value.toCharArray();
@@ -209,12 +209,12 @@ public class HTMLElementImpl
         }
         return value;
     }
-
+    
     /**
      * Convenience method used to capitalize a one-off attribute value before it
      * is returned. For example, the align values "LEFT" and "left" will both
      * return as "Left".
-     *
+     * 
      * @param name The name of the attribute
      * @return The capitalized value
      */
@@ -222,7 +222,7 @@ public class HTMLElementImpl
         String    value;
         char[]    chars;
         int        i;
-
+        
         value = getAttribute( name );
         if ( value != null ) {
             // Convert string to charactares. Convert the first one to upper case,
@@ -239,14 +239,14 @@ public class HTMLElementImpl
         return value;
     }
 
-
+    
     /**
      * Convenience method returns the form in which this form element is contained.
      * This method is exposed for form elements through the DOM API, but other
      * elements have no access to it through the API.
      */
     public HTMLFormElement getForm() {
-        Node parent = getParentNode();
+        Node parent = getParentNode(); 
         while ( parent != null ) {
             if ( parent instanceof HTMLFormElement ) {
                 return (HTMLFormElement) parent;
